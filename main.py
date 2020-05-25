79ce6c1a2f49c628086220849cc4d86ffce762db
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-05-25 18:12:21 +0200

    Defined routes

--- main.py
+++ main.py
@@ -4,3 +4,31 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# sign in page
+@app.route('/sign_in')
+def sign_in_page():
+    return 'Sign in here!'
+
+
+# overview of algorithms
+@app.route('/algorithms')
+def algorithms_page():
+    return 'algorithms page'
+
+
+# upload data page
+@app.route('/upload_data')
+def upload_data_page():
+    return 'Upload data here!'
+
+
+# preprocessing page
+def preprocessing_overview_page():
+    return 'Preprocess your data here!'
+
+
+# report page
+def report_page():
+    return 'Here you can find the report'
