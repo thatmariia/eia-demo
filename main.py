9344f932a888f45c4b75c386333a86f83ef9b2a5
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-05-25 19:36:17 +0200

    Setup all templates and finished routing

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import render_template
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='web/static',
+            template_folder='web/templates')
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html', title="Home")
 
 
 # sign in page
 @app.route('/sign_in')
 def sign_in_page():
-    return 'Sign in here!'
+    return render_template('sign_in.html', title="Sign in")
 
 
 # overview of algorithms
 @app.route('/algorithms')
 def algorithms_page():
-    return 'algorithms page'
+    return render_template('algorithms.html', title="Algorithms")
 
 
 # upload data page
 @app.route('/upload_data')
 def upload_data_page():
-    return 'Upload data here!'
+    return render_template('upload_data.html', title="Upload data")
 
 
 # preprocessing page
 @app.route('/preprocessing')
 def preprocessing_overview_page():
-    return 'Preprocess your data here!'
+    return render_template('preprocessing.html', title="Preprocessing")
 
 
 # report page
 @app.route('/feedback_report')
 def report_page():
-    return 'Here you can find the report'
+    return render_template('feedback_report.html', title="Feedback Report")
