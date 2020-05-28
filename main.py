64c1a713ebf5d6ad68b6d9c97ea7246317096314
Mariia Turchina <mariia.turchina@gmail.com>
2020-05-28 14:03:42 +0200

    added another home super hardcoded

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__,
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', title="Home")
+    return render_template('home2.html', title="Home") # changed to home2 for now
 
 
 # sign in page
