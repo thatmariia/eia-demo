908ccd71d9129e7bd1571e425ddad9e6b859bc6e
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-05-30 12:06:38 +0200

    udpated a bit more

--- Algorithm.py
+++ Algorithm.py
@@ -1,9 +1,8 @@
-
-
 class Algorithm:
     """
     Just for the overview of algorithms
     """
+
     def __init__(self, name, description):
         self.name = name
         self.description = description
@@ -12,9 +11,15 @@ class Algorithm:
 def get_algorithms() -> [Algorithm]:
     # TODO:: put actual legit algorithms here
     algorithms_row_1 = [
-            Algorithm("blah1", "blah blah 1"),
-            Algorithm("blah2", "blah blah 2"),
-            Algorithm("blah3", "blah blah 3")
+        Algorithm("blah1", "blah blah 1"),
+        Algorithm("blah2", "blah blah 2"),
+        Algorithm("blah3", "blah blah 3")
     ]
 
-    return [algorithms_row_1]
+    algorithms_row_2 = [
+        Algorithm("blah4", "blah blah 4"),
+        Algorithm("blah5", "blah blah 5"),
+        Algorithm("blah6", "blah blah 6")
+    ]
+
+    return [algorithms_row_1, algorithms_row_2]
